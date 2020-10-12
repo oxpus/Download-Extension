2d8342dc30f6f7868740c08b123b3c6a297d0dba
Karsten Ude <oxpus@users.noreply.github.com>
2020-10-12 20:17:35 +0200

    Update release_8_1_3.php

--- oxpus/dlext/migrations/v810/release_8_1_3.php
+++ oxpus/dlext/migrations/v810/release_8_1_3.php
@@ -29,51 +29,6 @@ class release_8_1_3 extends \phpbb\db\migration\migration
 		return [
 			// Set the current version
 			['config.update', ['dl_ext_version', $this->dl_ext_version]],
-
-			['custom', [[$this, 'create_htaccess']]],
 		];
-	}
-
-	public function create_htaccess()
-	{
-		// define extension folder constants
-		$dl_ext_filebase_path = $this->phpbb_root_path . $this->config['upload_path'] . '/dlext/';
-
-		$this->_create_folder($dl_ext_filebase_path . 'thumbs/');
-		$this->_create_folder($dl_ext_filebase_path . 'version/');
-		$this->_create_folder($dl_ext_filebase_path . 'version/images/');
-
-		$this->_create_htaccess($dl_ext_filebase_path . 'thumbs/');
-		$this->_create_htaccess($dl_ext_filebase_path . 'version/images/');
-	}
-
-	public function _create_folder($path)
-	{
-		if (@file_exists($path))
-		{
-			return;
-		}
-
-		@mkdir($path);
-		@phpbb_chmod($path, CHMOD_ALL);
-
-		$f = fopen($path . 'index.htm', 'w');
-		fclose($f);
-	}
-
-	public function _create_htaccess($path)
-	{
-		if (!@file_exists($path))
-		{
-			return;
-		}
-
-		global $phpbb_extension_manager;
-
-		$ext_path = $phpbb_extension_manager->get_extension_path('oxpus/dlext', true);
-
-		@unlink($path . '.htaccess');
-		@copy ($ext_path . 'htaccess', $path . '.htaccess');
-		@phpbb_chmod($path . '.htaccess', CHMOD_READ);
 	}
 }
