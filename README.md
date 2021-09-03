d5f6d850f0b77863dd523a20b21656cc2b3b4f1f
Karsten Ude <oxpus@users.noreply.github.com>
2021-09-03 16:41:39 +0200

    Final Release 8.2.6

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Offers files for download based on an extensive rule set.
 
 Based on phpBB 3.2.x / 3.3.x<br />
 Original by Hotschi<br />
-Latest release 8.2.5<br />
+Latest release 8.2.6<br />
 (c) 2002 - 2021 OXPUS networks
 
 
---
+++ oxpus/dlext/migrations/v820/release_8_2_6.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ *
+ * @package   phpBB Extension - Oxpus Downloads
+ * @copyright 2002-2021 OXPUS - www.oxpus.net
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+namespace oxpus\dlext\migrations\v820;
+
+class release_8_2_6 extends \phpbb\db\migration\migration
+{
+	protected $dl_ext_version = '8.2.6';
+
+	public function effectively_installed()
+	{
+		return isset($this->config['dl_ext_version']) && version_compare($this->config['dl_ext_version'], $this->dl_ext_version, '>=');
+	}
+
+	public static function depends_on()
+	{
+		return ['\oxpus\dlext\migrations\v820\release_8_2_5'];
+	}
+
+	public function update_data()
+	{
+		return [
+			// Set the current version
+			['config.update', ['dl_ext_version', $this->dl_ext_version]],
+		];
+	}
+}
